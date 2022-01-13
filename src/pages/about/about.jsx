7c7be4e7c79fa05e545217d2eacc19e3716a69aa
icarodgl <icarodgl@gmail.com>
2022-01-13 17:06:30 -0300

    update

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,18 +1,29 @@
-import { Container, Row, Col, Image } from 'react-bootstrap';
-import perfil from '../../assets/perfil.jpg'
-import './about.css'
+import { Container, Row, Col, Image } from "react-bootstrap";
+import perfil from "../../assets/perfil.jpg";
+import "./about.css";
 export default function about() {
-    return (<Container>
-        <Row className="justify-content-md-center">
-            <Col xl="10">
-                <Image className="img-center" src={perfil} roundedCircle />
+  return (
+    <Container>
+      <Row className="justify-content-md-center">
+        <Col xl="10">
+          <Image className="img-center" src={perfil} roundedCircle />
 
-                <p className="text text-center"><br />
-                        Contato: Vera Gavaza<br />
-Telefone: (27) 9 99732190<br />
-Email: gavazzavera@gmail.com</p>
-            </Col>
-        </Row>
-        <Row ><Col className="espaco"></Col></Row>
-    </Container>)
+          <p className="text text-center text-larger">Contato: Vera Gavazza</p>
+          <p className="text text-center text-larger">Telefone: (27) 9 99732190</p>
+          <p className="text text-center text-larger">Email: gavazzavera@gmail.com</p>
+        </Col>
+      </Row>
+      <br />
+      <Row>
+        <p className="descricao text-larger">
+          Meu nome é Vera Gavazza, sou formada em pedagogia e pós graduada em
+          Hatha yoga e Arteterapia. Acredito que as pessoas se transformam
+          através do autoconhecimento e da educação. Para isto, busco conectar a
+          filosofia e prática do Hatha Yoga e a expressão em Arteterapia para o
+          alcance de uma vida mais simples e tranquila, contribuindo para o bem
+          estar pessoal e social.
+        </p>
+      </Row>
+    </Container>
+  );
 }
